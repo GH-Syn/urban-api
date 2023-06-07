cfe3f80454e7c3236e71221ed6334628664b660b
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 07:03:36 +1000

    tests: Add tests for user posts
    Adds type tests and abs tests for user
    
     - Introduces test to test that user email is an integer
     - Introduces test to test that user email is greater than or equal to 1
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,9 +5,9 @@ from uapi import User
 
 class TestUserModel(unittest.TestCase):
     """
-    - User needs to have a `name`
-    - `name` needs to be an instance of `str`
-    - User must have an email address
+    - User has `name`
+    - `name` is instance of `str`
+    - User has email address
     """
 
     def __init__(self, method_name) -> None:
@@ -15,28 +15,40 @@ class TestUserModel(unittest.TestCase):
 
     def setUp(self) -> None:
         """
-        Create an instance of `uapi.User`.
+        Create instance of `uapi.User`.
         """
         self.user = User(name="John Doe", posts=2, email="johndoe@example.com")
         return super().setUp()
 
     def test_name_type(self):
         """
-        Test name is an instance of `str`
+        name is an instance of `str`
         """
         self.assertIsInstance(self.user.name, str)
 
     def test_email_exists(self):
         """
-        Test that user has an email address
+        user has an email address
         """
         self.assertTrue(self.user.__getattribute__('email'))
 
     def test_email_type(self):
         """
-        Test email is an instance of `str`
+        email is an instance of `str`
         """
         self.assertIsInstance(self.user.email, str)
+
+    def test_posts_abs(self):
+        """
+        posts is an absolute value
+        """
+        self.assertTrue(self.user.posts >= 0)
+
+    def test_posts_type(self):
+        """
+        posts is an integer
+        """
+        self.assertIsInstance(self.user.posts, int)
 
 
 if __name__ == "__main__":
