d6fb375f5a16122393701b212176b95e6750774a
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 07:32:12 +1000

    implement: Stub and documentation for `utils.py`
    This commit adds a stub file `utils.py`
    
     - Introduces file
     - Implements docstring w/ license
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ uapi/utils.py
@@ -0,0 +1,8 @@
+"""
+license: Apache 2.0
+
+The three 'pillars' of `urban-api/uapi/utils.py`
+ - retrieve info with the `requests` module.
+ - format said info with `bs4` module.
+ - display said info with `rich` module.
+"""
