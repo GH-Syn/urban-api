87266f727d6c8c8b8d696468a0fa992e5ceb33f9
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 07:35:32 +1000

    docs: Add docstring to files, refactor `LICENSE`
    This commit adds docstrings to various files
    
     - Implements docstrings with description to each file
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- uapi/models.py
+++ uapi/models.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2023 Joshua Rose
+--------------------------
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+or in the `LICENSE` file at https://github.com/GH-Syn/urban-api/blob/main/LICENSE
+
+Description
+-----------
+This file holds models and classes that are needed.
+Any basic data structures will be found here.
+"""
+
 import dataclasses
 
 
--- uapi/utils.py
+++ uapi/utils.py
@@ -1,8 +1,16 @@
 """
-license: Apache 2.0
+Copyright 2023 Joshua Rose
+--------------------------
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+or in the `LICENSE` file at https://github.com/GH-Syn/urban-api/blob/main/LICENSE
+
+Description
+-----------
 The three 'pillars' of `urban-api/uapi/utils.py`
- - retrieve info with the `requests` module.
- - format said info with `bs4` module.
- - display said info with `rich` module.
+ 1. retrieve info with the `requests` module.
+ 2. format said info with `bs4` module.
+ 3. display said info with `rich` module.
 """
