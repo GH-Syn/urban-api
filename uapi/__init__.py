f1233f49efc362d082d0c6ee2e4849bc0ea570f1
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 09:38:28 +1000

    build: Add metadata and versioning
    This commit adds versioning preparing for initial release.
    
     - Introduces versioning, author, license, home
     - Changes versioning scheme
     - Implements docstring to constructor file
    
    Tag: 0.1.0
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- uapi/__init__.py
+++ uapi/__init__.py
@@ -1,1 +1,15 @@
+"""
+Copyright 2023 Joshua Rose
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+or in the `LICENSE` file at https://github.com/GH-Syn/urban-api/blob/main/LICENSE
+"""
+
+__license__ = "Apache 2.0"
+__author__ = "Joshua Rose"
+__home__ = "https://github.com/GH-Syn/urban-api"
+__version__ = "0.1.0"
+
 from .models import User  # pyright: ignore
