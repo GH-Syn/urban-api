ce75deddc621499e6237bd19cb535aaa21b6d717
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 07:45:28 +1000

    docs: Generate docs with 'Sphinx'
    Generate documentation with sphinx
    
     - Changes `conf.py` to also include `../uapi'
     - Implements path with `sys`
     - add tag `0.0.0-devel`
    
    Tag: 0.0.0-devel
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+
+sys.path.insert(0, "../uapi")
+sys.path.insert(0, "../uapi/")
+
+project = "Urban API"
+copyright = "2023, Joshua Rose"
+author = "Joshua Rose"
+release = "0.0.0-devel"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
