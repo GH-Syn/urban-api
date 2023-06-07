b47374f630fa94294e63967b76d14e689c2651b3
Joshua Rose <joshuarose099@gmail.com>
2023-06-08 09:43:03 +1000

    feat: Add `setup.py` file
    This commit adds a setup file
    
     - Introduces default setup file
     - Changes default versioning
     - Implements setup requirements
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="uapi",
+    version="0.1.0",
+    description="Urban dictionary Python library",
+    author="Joshua Rose",
+    author_email="joshuarose099@gmail.com",
+    packages=["uapi"],
+)
